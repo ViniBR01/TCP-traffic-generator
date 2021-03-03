0bb683e688e844c2fb65aaddcb6abb4e16a9e0f2
ViniBR01 <vinicius@rice.edu>
2021-03-03 14:39:53 -0600

    Update default IP address to use loopback

--- client.c
+++ client.c
@@ -49,7 +49,7 @@ int client(options_t *options) {
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(portn);
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "192.168.0.108", &serv_addr.sin_addr)<=0)
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
         {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
