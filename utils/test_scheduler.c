e63ab3cdef3c8d27966293e0340c9e0bd8efe3e5
ViniBR01 <vinicius@rice.edu>
2021-03-08 16:37:25 -0600

    Write test for the kill_task method

--- utils/test_scheduler.c
+++ utils/test_scheduler.c
@@ -6,6 +6,7 @@
 #include "scheduler.h"
 
 void periodic_task(void *p, unsigned int task_id);
+void task_to_kill_task(void *p, unsigned int task_id);
 
 int main() {
     printf("Starting test: task scheduler.\n");
@@ -17,7 +18,11 @@ int main() {
 
     unsigned int *task_parameter2 = (unsigned int *) malloc(sizeof(unsigned int));
     *task_parameter2 = 1000000;
-    create_task(periodic_task, (void *)task_parameter2, STATE_READY, -1);
+    unsigned int my_id = create_task(periodic_task, (void *)task_parameter2, STATE_READY, -1);
+
+    unsigned int *task_parameter3 = (unsigned int *) malloc(sizeof(unsigned int));
+    *task_parameter3 = my_id;
+    create_task(task_to_kill_task, (void *)task_parameter3, STATE_WAITING, 5500000);
 
     //Now start scheduler
     while(1) {
@@ -35,4 +40,11 @@ void periodic_task(void *p, unsigned int task_id) {
     // printf("pointer address: %p\n\n", *(unsigned int *)p);
     delay(*(unsigned int *)p, task_id);
     return;
+}
+
+void task_to_kill_task(void *p, unsigned int task_id) {
+    printf("Executing the task to kill one task. My id: %d.\n", task_id);
+    printf("\tkill task with id: %d\n", *(unsigned int *)p);
+    kill_task(*(unsigned int *)p);
+    halt_me(task_id);
 }
