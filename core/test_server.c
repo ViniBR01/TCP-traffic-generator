6b5afa6d64de9aaec9772f7ddab28cef07b1b851
ViniBR01 <vinicius@rice.edu>
2021-03-09 07:40:52 -0600

    Create file to test server.c

---
+++ core/test_server.c
@@ -0,0 +1,12 @@
+/* Script to test the server implementation */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "server.h"
+#include "scheduler.h"
+
+int main() {
+    printf("Starting test: server.c.\n");
+    return 0;
+}
