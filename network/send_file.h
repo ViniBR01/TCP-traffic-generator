efc4378aa220aff5d9a56e1cbb90e2a2aef5e37a
ViniBR01 <vinicius@rice.edu>
2021-03-10 08:18:31 -0600

    Change intreface to pass by reference

--- network/send_file.h
+++ network/send_file.h
@@ -2,10 +2,11 @@
 
 typedef struct {
     int file_size;
+    int max_chunk_size;
     unsigned int remote_addr;
     unsigned short remote_port;
 } file_t;
 
-void * start_file_transfer(file_t file_info);
+void * start_file_transfer(file_t *file_info);
 int send_file_chunk(void * file_status);
 int clean_up_file_transfer(void * file_status);
