af2b052fa490fc3ccd678ef6ad09daac3128ae8c
ViniBR01 <vinicius@rice.edu>
2021-03-10 07:37:40 -0600

    Write first test for server

--- core/test_server.c
+++ core/test_server.c
@@ -1,4 +1,6 @@
 /* Script to test the server implementation */
+//to compile:
+//gcc -o test test_server.c server.c ./../utils/scheduler.c -I. -I./../utils
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,5 +10,7 @@
 
 int main() {
     printf("Starting test: server.c.\n");
+    options_t options = { 0, 8080, "", 1000, 10 };
+    server(&options);
     return 0;
 }
