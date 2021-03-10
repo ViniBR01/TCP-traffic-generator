b20ba4e24b80c60513c8694883d034a0fa589123
ViniBR01 <vinicius@rice.edu>
2021-03-10 10:52:24 -0600

    Fix return types; write temporary tests

--- network/send_file.c
+++ network/send_file.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -16,7 +17,7 @@ typedef struct {
     char *send_buffer;
 } file_status_t;
 
-int start_file_transfer(void *file_info_in, unsigned int task_id){
+void start_file_transfer(void *file_info_in, unsigned int task_id){
     file_t *file_info = (file_t *) file_info_in;
     file_status_t *file_status = 
         (file_status_t *) malloc(sizeof(file_status_t));
@@ -48,7 +49,8 @@ int start_file_transfer(void *file_info_in, unsigned int task_id){
     struct sockaddr_in sin;
     memset (&sin, 0, sizeof (sin));
     sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = file_info->remote_addr;
+    //sin.sin_addr.s_addr = file_info->remote_addr;
+    sin.sin_addr.s_addr = 16777343; // XXX Fix-me: parsing the remote_addr has a bug
     sin.sin_port = htons(file_info->remote_port);
 
     /* connect to the server */
@@ -58,6 +60,39 @@ int start_file_transfer(void *file_info_in, unsigned int task_id){
         /* NOTREACHED */
     }
 
+    //Testing up to this point: communicate with the example server
+    /* everything looks good, since we are expecting a
+     message from the server in this example, let's try receiving a
+     message from the socket. this call will block until some data
+     has been received */
+    printf("This socket has id: %d\n", my_socket);
+    char *buffer;
+    int size = 500;
+    buffer = (char *) malloc(size);
+    int count = recv(my_socket, buffer, size, 0);
+    if (count < 0)
+        {
+        perror("receive failure");
+        abort();
+        }
+
+    /* in this simple example, the message is a string, 
+        we expect the last byte of the string to be 0, i.e. end of string */
+    if (buffer[count-1] != 0)
+        {
+        /* In general, TCP recv can return any number of bytes, not
+        necessarily forming a complete message, so you need to
+        parse the input to see if a complete message has been received.
+            if not, more calls to recv is needed to get a complete message.
+        */
+        printf("Message incomplete, something is still being transmitted\n");
+        } 
+    else
+        {
+        printf("Here is what we got: %s", buffer);
+        }
+    ///////////////////////////////////////////////////////////////////////
+
     /* Send the first chunck with size information */
     // XXX write the logic to send first chunk with small header
     // file_status->already_sent += sent_bytes;
@@ -65,14 +100,15 @@ int start_file_transfer(void *file_info_in, unsigned int task_id){
     //Schedule the send another chunk task for this file here??? Yes!
 
     //Kill this task
+    kill_task(task_id);
 
-    return EXIT_SUCCESS;
+    return;
 }
 
-int send_file_chunk(void *file_status, unsigned int task_id) {
-    return EXIT_SUCCESS;
+void send_file_chunk(void *file_status, unsigned int task_id) {
+    return;
 }
 
-int clean_up_file_transfer(void *file_status, unsigned int task_id) {
-    return EXIT_SUCCESS;
+void clean_up_file_transfer(void *file_status, unsigned int task_id) {
+    return;
 }
--- network/send_file.h
+++ network/send_file.h
@@ -8,6 +8,6 @@ typedef struct {
     unsigned short remote_port;
 } file_t;
 
-int start_file_transfer(void *file_info_in, unsigned int task_id);
-int send_file_chunk(void *file_status, unsigned int task_id);
-int clean_up_file_transfer(void *file_status, unsigned int task_id);
+void start_file_transfer(void *file_info_in, unsigned int task_id);
+void send_file_chunk(void *file_status, unsigned int task_id);
+void clean_up_file_transfer(void *file_status, unsigned int task_id);
