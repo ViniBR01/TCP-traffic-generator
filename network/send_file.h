1e2c247360eea894d111c4c46d7f632fedc13909
ViniBR01 <vinicius@rice.edu>
2021-03-09 18:44:07 -0600

    Define send_file interface

--- network/send_file.h
+++ network/send_file.h
@@ -1,1 +1,11 @@
 /* send_file.h - TCP traffic generator */
+
+typedef struct {
+    int file_size;
+    unsigned int remote_addr;
+    unsigned short remote_port;
+} file_t;
+
+void * start_file_transfer(file_t file_info);
+int send_file_portion(void * file_status);
+int clean_up_file_transfer(void * file_status);
