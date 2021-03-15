9e3f6cb4255d3704dc9f96bfe2fd4a125c5ce228
ViniBR01 <vinicius@rice.edu>
2021-03-14 20:55:55 -0500

    Define the interface of config_messages

--- network/config_messages.h
+++ network/config_messages.h
@@ -1,3 +1,18 @@
 /* config_messages.h - TCP traffic generator */
 
-void send_silly_message();
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <string.h>
+
+#define MSGTYPE_SETUP 1
+#define MSGTYPE_DIGEST 2
+
+typedef struct {
+    uint8_t message_type;
+    uint32_t message_length;
+    char *buffer;
+} message_t;
+
+message_t * cook_message(u_int8_t type, u_int32_t length, char *string_message);
+void send_message(void *cooked_message, unsigned int task_id);
