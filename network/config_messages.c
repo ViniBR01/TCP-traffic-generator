02c4d904eb4eb901ed74b19e69bd188415b33bbc
ViniBR01 <vinicius@rice.edu>
2021-03-14 20:56:43 -0500

    Write boilerplate for cook_message

--- network/config_messages.c
+++ network/config_messages.c
@@ -1,5 +1,23 @@
 /* config_messages.c - TCP traffic generator */
 
-void send_silly_message() {
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include "config_messages.h"
+
+message_t * cook_message(u_int8_t type, u_int32_t length, char *string_message) {
+    message_t *message_arg = (message_t *) malloc(sizeof(message_t));
+    message_arg->message_type = type;
+    message_arg->message_length = length;
+    message_arg->buffer = (char *)malloc(length * sizeof(char));
+
+    //XX Here, fill buffer with silly message and header
+
+    return message_arg;
+}
+void send_message(void *cooked_message, unsigned int task_id) {
+    
     return;
 }
