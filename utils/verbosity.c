3b400ab53415020cbcbe8ce6336d7b864d72dfcf
ViniBR01 <vinicius@rice.edu>
2021-03-22 14:53:18 -0500

    Implement vebosity module

--- utils/verbosity.c
+++ utils/verbosity.c
@@ -1,1 +1,28 @@
 /* verbosity.c - TCP traffic generator */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "verbosity.h"
+
+static u_int32_t level;
+
+int set_verbosity(u_int32_t new_level) {
+    level = new_level;
+    return EXIT_SUCCESS;
+}
+
+int verbosity(char *message, uint32_t message_level) {
+    if (level >= message_level) {
+        printf(message);
+        return EXIT_SUCCESS;
+    } else {
+        return EXIT_FAILURE;
+    }
+    
+}
+
+u_int32_t get_verbosity {
+    return level;
+}
--- utils/verbosity.h
+++ utils/verbosity.h
@@ -1,1 +1,13 @@
 /* verbosity.h - TCP traffic generator */
+
+#ifndef _VERBOSITY_H
+#define _VERBOSITY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int set_verbosity(u_int32_t new_level);
+int verbosity(char *message, uint32_t message_level);
+u_int32_t get_verbosity();
+
+#endif  /* _VERBOSITY_H */
