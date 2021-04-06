9e98c5d91fa0e3849b0f3003f17bae09fb144c57
ViniBR01 <vinicius@rice.edu>
2021-04-06 08:49:41 -0500

    Turn clean_data into reusable function

--- output/clean_data.py
+++ output/clean_data.py
@@ -5,18 +5,19 @@ import pandas as pd
 from scipy import stats
 import numpy as np
 
-data_in = [0, 1, 2, 3, 4, 5, 67, 6, 7, 8, 9]
+data_in = [0, 1, 2, 3, 4, 5, 67, 6, 7, 8, 9, 10000]
 filename = 'download-times-DL-10000ms-10kB-1617663388.txt'
-#data_in = pd.read_csv(filename, header = None)
+# data_in = pd.read_csv(filename, header = None)
 print(data_in)
 
-z = np.abs(stats.zscore(data_in))
-print(z)
-
-data_out = np.array(data_in)
-z_th = 3
-while(np.max(z) > z_th):
-    data_out = data_out[z<z_th]
-    print(data_out)
+def clean_data(data_in):
+    data_out = np.array(data_in)
     z = np.abs(stats.zscore(data_out))
+    z_th = 3
+    while(np.max(z) > z_th):
+        data_out = data_out[z<z_th]
+        #print(data_out)
+        z = np.abs(stats.zscore(data_out))
+    return data_out
 
+print(clean_data(data_in))
